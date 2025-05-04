e07c3f063707e5c41cfba16ff695704b863bf231
mgbrouli <wtnbrouli@gmail.com>
2025-05-04 19:56:21 -0300

    Atualizando a tela de dash com estilo e separando os slot das task, criei também um exemplo de task que sera gerada a partir da listas em um banco de dados.

---
+++ front-end/src/static/taskManagement.js
@@ -0,0 +1,18 @@
+class TaskManegement{
+    constructor(id, name, description, status){
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.status = status;
+    }
+
+
+}
+
+
+const addTask = document.getElementById("addTask");
+const taskSlot  = document.getElementById("TaskInLine");
+
+addTask.addEventListener("click", ()=>{
+    
+})
