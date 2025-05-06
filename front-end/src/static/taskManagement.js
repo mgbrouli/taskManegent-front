80ba71a153d85d5556f140d98b675be9e221970a
mgbrouli <wtnbrouli@gmail.com>
2025-05-05 22:51:24 -0300

    Atualizaação do drag and drop com js

--- front-end/src/static/taskManagement.js
+++ front-end/src/static/taskManagement.js
@@ -13,6 +13,43 @@ class TaskManegement{
 const addTask = document.getElementById("addTask");
 const taskSlot  = document.getElementById("TaskInLine");
 
-addTask.addEventListener("click", ()=>{
-    
-})
+const taskBefore = document.getElementById("tasks_before");
+const taskMeddle = document.getElementById("tasks_middle");
+const taskEnded = document.getElementById("tasks_ended");
+const tasks = document.getElementsByClassName("task-obj");
+
+
+    for(tasked of tasks){
+        tasked.addEventListener("dragstart", (e)=>{
+            let selected = e.target
+
+            taskBefore.addEventListener("dragover", (e)=>{
+                e.preventDefault();
+            })
+            taskBefore.addEventListener("drop", (e)=>{
+                taskBefore.appendChild(selected);
+                selected = null;
+            })
+
+            ////////////////////////////////////////
+            taskMeddle.addEventListener("dragover", (e)=>{
+                e.preventDefault();
+            })
+            taskMeddle.addEventListener("drop", (e)=>{
+                taskMeddle.appendChild(selected);
+                selected = null;
+            })
+
+            /////////////////////////////////////////
+            taskEnded.addEventListener("dragover", (e)=>{
+                e.preventDefault();
+            })
+            taskEnded.addEventListener("drop", (e)=>{
+                taskEnded.appendChild(selected);
+                selected = null;
+            })
+
+        })
+    }
+
+
